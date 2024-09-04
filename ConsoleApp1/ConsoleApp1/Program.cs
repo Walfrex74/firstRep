77ba1ecc5a8d424aac95b30f5045ab544739e8b4
Walfrex <michailhaziev74@gmail.com>
2024-09-04 15:29:45 +0500

    task2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,15 +12,10 @@ namespace ConsoleApp1
         {
             Random r = new Random();
             int[] ints = new int[10];
-            int sum = 0;
             int len = ints.Length;
-            int col = 0;
 
             Console.WriteLine($"Максимальное число: " + max(3, 4, 5, 0));
             Console.WriteLine($"Катангенс: " + ctg(2));
-
-            Console.WriteLine($"Сумма массива с рандомными числами: " + arr(ints, sum, r));
-            Console.WriteLine($"Колличество элементов массива: " + arr_col(ints, col));
 
             Console.ReadKey();
         }
@@ -36,26 +31,6 @@ namespace ConsoleApp1
         static double ctg(int one)
         {
             return Math.Cos(one) / Math.Sin(one);
-        }
-
-        public static int arr(int[] ints, int sum, Random r)
-        {
-            for (int i = 0; i < ints.Length; i++)
-            {
-                ints[i] = r.Next(100);
-                sum += ints[i];
-            }
-            return sum;
-        }
-
-        static int arr_col(int[] ints, int col)
-        {
-            for (int i = 0; i < ints.Length; i++)
-            {
-                col++;
-            }
-
-            return col;
         }
     }
 }
