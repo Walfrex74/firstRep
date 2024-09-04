d7ae0e400d341ce3a7cbdc5ddcd6584928b3b9b0
Walfrex <michailhaziev74@gmail.com>
2024-09-04 22:51:18 +0500

    console foreground text

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green; 
+            
             Random r = new Random();
             int[] ints = new int[10];
             int sum = 0;
